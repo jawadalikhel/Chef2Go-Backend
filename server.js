14eb147409d7ce38c0760893488d7a0b0832d119
Your Name <jawadalikhel18@gmail.com>
2019-01-24 13:30:38 -0600

    connected to the database mongodb

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session')
 const authController = require('./controllers/authController');
+require('./db/db');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: true}
+}))
 
 
 app.use('/auth', authController);
 
-app.listen(9000, () =>{
+app.listen(process.env.PORT || 9000, ()  =>{
   console.log('Server is live on 9000');
 })
