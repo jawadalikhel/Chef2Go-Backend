25ae1707583849a5eb0e7a50bfbd85df99e3466d
Your Name <jawadalikhel18@gmail.com>
2019-01-31 00:36:00 -0600

    continue working on trying to display chef and coustomer profile and can edit them

--- controllers/chefController.js
+++ controllers/chefController.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Chef = require('../models/chefSchema.js');
 
+router.post('/dashboard', (req, res, next) => {
+  console.log(req.session, 'this is req.session')
+  if (req.session && req.session.user) {
+    Chef.findOne({ username: req.session.user.username }, function(err, user) {
+      if (user) {
+        req.user = user;
+        delete req.user.password; // delete the password from the session
+        req.session.user = user;  //refresh the session value
+        res.locals.user = user;
+      }
+      // finishing processing the middleware and run the route
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
 
 router.get('/chefs', async (req, res) =>{
   try {
@@ -19,6 +37,24 @@ router.get('/chefs', async (req, res) =>{
   }
 })
 
+router.get('/chefs/:profile', async(req, res) =>{
+  try {
+    if(req.session.logged){
+
+    } else {
+      res.json({
+        status: 401,
+        data: 'login required'
+      })
+    }
+  } catch (err) {
+    res.json({
+      status: 401,
+      data: err
+    })
+  }
+})
+
 
 
 router.get('/chefs/:id', async(req, res) =>{
@@ -28,6 +64,7 @@ router.get('/chefs/:id', async(req, res) =>{
       status: 200,
       data: getChef
     })
+    console.log(getChef, ' this is getChef')
   } catch (err) {
     console.log(err, ' this is error in /:id')
   }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const session = require('express-session');
+// const session = require('express-session');
+const session = require('client-sessions');
 const cors = require('cors');
 const authController = require('./controllers/authController');
 const chefController = require('./controllers/chefController');
@@ -11,12 +12,19 @@ require('./db/db');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// app.use(session({
+//   secret: 'work hard',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {secure: true}
+// }))
+
 app.use(session({
-  secret: 'work hard',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {secure: true}
-}))
+  cookieName: 'session',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
 
 const corsOptions = {
   origin: 'http://localhost:3000',
